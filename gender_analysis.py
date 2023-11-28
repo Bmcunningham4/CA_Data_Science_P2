5600eedfc7116637adc7d6a937fbdee1dffea02c
Ben Cunningham <bmcunningham4@outlook.com>
2023-11-28 19:14:57 +0800

    Finished Scoping - Starting data cleaning

---
+++ gender_analysis.py
@@ -0,0 +1,13 @@
+
+#? This file will contain:
+"""
+Gender Pay Gap Analysis:
+Explore salary differences between genders across:
+- different job titles, 
+- countries
+- seniority levels. 
+
+#* Most importantly
+Identify if there's a significant gap and in what contexts it's most prominant!
+"""
+
---
+++ project_plan.py
@@ -0,0 +1,17 @@
+
+#? This time compared to first project, going to have some key differences!
+"""
+1) Going to include a lot more visualisations of the data, eg. using seaborn module!
+
+2) Going to include multiple files with different things being investigated!
+"""
+
+#* 3 Files that I'm going to explore!
+"""
+1) Gender Pay Gap analysis!
+2) Geographic Salary variations!
+3) Predictive Modeling for salary!
+
+4) Experience vs Salary! #? If I have time for this one but 3 is a good amount to aim for!
+
+"""
