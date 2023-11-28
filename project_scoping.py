e826196b9eb9698deeccd05b868d167ea4f00f7a
Ben Cunningham <bmcunningham4@outlook.com>
2023-11-28 17:56:00 +0800

    Early notes and CSV file uploaded

---
+++ project_scoping.py
@@ -0,0 +1,54 @@
+
+#? Straight from CodeAcad, Project scoping:
+"""
+Let's plan this guy out: (Accoring to university of Chicago's Data Science scoping guide, 3 main compononets)
+
+- Goals
+- Data
+- Analysis 
+
+"""
+
+#* Yikes this project is gonna be a lot harder because I have to find data online and no example to compare to woahhh
+
+#? Collecting Data: (websites)
+"""
+data.gov ------- boring as bloody batshittt..
+kaggle   ------- This has some interesting stuff!! NBA Stats, spotify.., animal condition..
+data.world
+awesomedata
+Fivethirtyeight
+Buzzfeed
+UCI
+"""
+
+#? Potential Ideas:
+"""
+Kaggle
+- 2023 Holidays DataSet
+- Salary by job title and Country #* Winner winner Chicken Dinner
+- Animal Condition
+- World's top-50 playlist musicality data
+- Trending YouTube Video Statistics (last updated 4 years ago)
+- Chess Game Dataset (Lichess)
+
+The rest of the game below Kaggle look okay data.world looks like it's most AI orientated but in a do it for you sorta way.. 
+& the others... look alright mostly github stashes
+"""
+
+#? Need to think of ideas to investigate not just what happens to this variable as this variable changes...
+"""
+I'm liking the look of the Salary by Job Title and Country dataset
+
+Some Potential Ideas..
+- 
+"""
+
+
+
+
+# Look at .png file!!!! That's how you get screenshots in here fam and can alter them and edit them innit
+"""
+Hopefully that can be a mini project for me machine learning algorithm to make all my photos position my head in the same spot
+By recognising the position of the ice bath!!!
+"""
