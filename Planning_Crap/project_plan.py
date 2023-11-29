67996a86c48d0574eeabb57e47cec3d93c65b0f6
Ben Cunningham <bmcunningham4@outlook.com>
2023-11-29 09:34:23 +0800

    Start of gender analysis

---
+++ Planning_Crap/project_plan.py
@@ -0,0 +1,35 @@
+
+#? This time compared to first project, going to have some key differences!
+"""
+1) Going to include a lot more visualisations of the data, eg. using seaborn module!
+
+2) Going to include multiple files with different things being investigated!
+"""
+
+#* 3 Files that I'm going to explore!
+"""
+1) Gender Pay Gap analysis!
+2) Geographic Salary variations!
+3) Predictive Modeling for salary!
+
+4) Experience vs Salary! #? If I have time for this one but 3 is a good amount to aim for!
+
+"""
+
+
+#? notes, as I'm going:
+"""
+Should I make a file with all the basic analysis for all the stats..
+eg. median salary for software eng..
+    median salary for Australian...
+    Then avg salary for an Australian..
+
+#* But maybe once I've got all that ^^ Put it in nice tables to summarise it all
+AND REMEMBER -- I'm not doing this for every basic possible stat I'm doing this for all the gender related stats!!
+
+
+Don't need to do this crap below ...
+That sorta crap and also do I include Lots of different dataframes made or do I just get that crap from the 1 main dataframe..
+"""
+
+
--- gender_analysis.py
+++ gender_analysis.py
@@ -11,3 +11,38 @@ Explore salary differences between genders across:
 Identify if there's a significant gap and in what contexts it's most prominant!
 """
 
+# Little note for learning what granularity means in data:
+"""
+High granularity basically means very detailed and includes things like every transaction in a retail store, or temperature readings from different sensors every second
+Low Granularity means the opposite and includes things like only the monthly sails for a retail store or Annual average temp..
+
+High granularity can potenitally go into deeper analysis but can sometimes be overwhelming or too hard to extract insights from..
+Low Granularity can be good sometimes easier to manage and manipulate but can't lead to as deep insights or analysis...
+"""
+
+#? There isn't much to know about cleaning and checking data from what I could find:
+#? So without further adue, let's begin your analysis benno...
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import csv
+#? Only other shite I'll have to import is the skits. stats crap..
+
+salary_df = pd.read_csv("Salary.csv")
+# print(salary_df) ---- data looks good doesn't even look like it needs any cleaning...
+
+#! Start of summary info
+
+#! mean
+#* Most basic: Avg salary for male/ female no context!
+mean_female_salary = salary_df[salary_df["Gender"] == "Female"].Salary.mean() #* I just realised I actually don't know what currency I'm dealing with in here!!
+mean_male_salary = salary_df[salary_df["Gender"] == "Male"].Salary.mean()
+
+print(f"The average male salary is ${round(mean_male_salary, 2)}")
+print(f"The average female salary is ${round(mean_female_salary, 2)}")
+mean_salary_diff = round(mean_male_salary - mean_female_salary,2)
+
+print(f"The mean salary difference is + ${mean_salary_diff} towards the male", '\n')
+
+
