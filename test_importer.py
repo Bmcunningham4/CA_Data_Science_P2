eda8dbb936600a29c8248aa2021589e55bdbe32e
Ben Cunningham <bmcunningham4@outlook.com>
2023-12-05 09:29:31 +0800

    Need to save more, since cells out here deleting themselves..

--- test_importer.py
+++ test_importer.py
@@ -3,10 +3,6 @@ file_path1 = "/Users/bencunningham/Documents/GitHub/CodeAcademy_Project_2/test_i
 file_path2 = "/Users/bencunningham/Documents/GitHub/CodeAcademy_Project_2/basic_data.ipynb"
 
 
-import sys
-# print(sys.path)
-print(sys.version)
-# import basic_data 
 
 
 import basic_data 
