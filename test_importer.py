922e71aab8e3fac0a1fda412472baec24d2f4439
Ben Cunningham <bmcunningham4@outlook.com>
2023-12-05 17:45:36 +0800

    Deleting merge conflict

--- test_importer.py
+++
@@ -1,7 +0,0 @@
-
-file_path1 = "/Users/bencunningham/Documents/GitHub/CodeAcademy_Project_2/test_importer.py"
-file_path2 = "/Users/bencunningham/Documents/GitHub/CodeAcademy_Project_2/basic_data.ipynb"
-
-
-
-import basic_data 
