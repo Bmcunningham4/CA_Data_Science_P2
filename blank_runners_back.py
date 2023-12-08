78a4ff117835f9f854dc56472aaab11c9c07587d
Ben Cunningham <bmcunningham4@outlook.com>
2023-12-08 15:02:30 +0800

    Finished Age -> Race boxplots

--- blank_runners_back.py
+++ blank_runners_back.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
+import re
 
 salary_df = pd.read_csv("Salary.csv")
 male_df = salary_df[salary_df.Gender == "Male"]
@@ -31,3 +32,38 @@ print(round(USA_mean_diff))
 print(round(uk_mean_diff))
 print(round(Australia_mean_diff))
 print(round(China_mean_diff))
+
+
+
+
+
+#? This crap is separate but includes the 25 job titles that the boolean doesn't catch
+def job_title_funct(title):
+    tech_keywords = re.compile(r'\bsoftware\b|\bdeveloper\b|\bengineer\b|\bit|\bdata\b', flags=re.IGNORECASE)
+    manager_keywords = re.compile(r'\bmanager\b|\bdirector\b|\bceo\b|\bvp|\boperations\b', flags=re.IGNORECASE)
+    marketing_keywords = re.compile(r'\bmarketing\b|\banalyst\b|\bcoordinator\b|\bspecialist|\bdigital\b', flags=re.IGNORECASE)
+    finance_keywords = re.compile(r'\bfinancial\b|\banalyst\b|\baccountant\b|\badvisor|\bintelligence\b', flags=re.IGNORECASE)
+    creative_keywords = re.compile(r'\bdesigner\b|\bcreative\b|\bcontent\b|\bgraphic|\bux\b', flags=re.IGNORECASE)
+    
+    if any(re.search(tech_keywords, title) for title in [title]):
+        return "Technology"
+    elif any(re.search(manager_keywords, title) for title in [title]):
+        return "Management"
+    elif any(re.search(marketing_keywords, title) for title in [title]):
+        return "Marketing"
+    elif any(re.search(finance_keywords, title) for title in [title]):
+        return "Finance"
+    elif any(re.search(creative_keywords, title) for title in [title]):
+        return "Creative"
+    else:
+        return title  # Returning the title itself for uncategorized titles
+
+# Assuming salary_df is your DataFrame containing job titles and salaries
+salary_df["Job Type"] = salary_df["Job Title"].apply(job_title_funct)
+
+# Extracting and printing unique uncategorized job titles
+uncategorized_titles = salary_df[salary_df["Job Type"] == salary_df["Job Title"]]["Job Title"].unique()
+print("Uncategorized Job Titles:")
+print(len(uncategorized_titles))
+for job in uncategorized_titles:
+    print(job)
